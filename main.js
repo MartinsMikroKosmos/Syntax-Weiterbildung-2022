8b4c00dfa175b3571023180c3f6d29edd93f7f19
Martinus Iudex <martin.richter.1990@gmail.com>
2022-04-12 14:07:00 +0200

    JavaScript hinzugefügt

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+function helloWorld() {
+// This line prints a simple message console.log("Hello World");
+    // Return value
+    return 0;
+}
+
 function main()  {
     // This line prints a simple message
     console.log("Calling the function:  ");
