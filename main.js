975524cdaea62b664146ed8ec5bf84ac83375c98
Martinus Iudex <103416201+MartinsMikroKosmos@users.noreply.github.com>
2022-04-12 12:54:48 +0200

    Update main
    Funktion geändert

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function helloWorld()  {
+function main()  {
     // This line prints a simple message
-    console.log("Hello World");
-
+    console.log("Calling the function:  ");
+    // function call
+    helloWorld();
+    
     // return value
     return 0;
 }
